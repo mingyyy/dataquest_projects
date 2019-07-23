e2c5b0aada7b08b451cee148c0e104783e4c288e
Mingyyy <j.yanming@gmail.com>
2019-07-23 11:14:52 +0800

    Add upvotes.py

---
+++ py/upvotes.py
@@ -0,0 +1,23 @@
+import reduce
+
+
+def count_upvote():
+    df = reduce.load_data()
+    upvotes = df["upvotes"].value_counts(sort=True, ascending=False)
+    counter = 0
+    for name, row in upvotes.items():
+        counter += 1
+        if counter <= 10:
+            print("{0}:{1}".format(name, row))
+
+
+def max_upvote():
+    df = reduce.load_data()
+    df.sort_values("upvotes", inplace=True)
+    print(df.head(10))
+    return df
+
+
+if __name__ == "__main__":
+    # count_upvote()
+    max_upvote()
