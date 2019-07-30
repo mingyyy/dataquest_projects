c5c580e0eac219e4a519f352a28c339d934fb528
Mingyyy <j.yanming@gmail.com>
2019-07-30 20:52:11 +0200

    Add Problem_7

--- euler/Problem_6-Sum_square_difference.py
+++ euler/Problem_6-Sum_square_difference.py
@@ -5,6 +5,7 @@ Hence the difference between the sum of the squares of the first ten natural num
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 """
 
+
 def sum_sqrt(n, p):
     s = 0
     for i in range(1, n+1):
--- euler/Problem_7-10001st_prime.py
+++ euler/Problem_7-10001st_prime.py
@@ -1,4 +1,25 @@
 """ 
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-What is the 10 001st prime number?
+What is the 10001st prime number?
 """
+
+def isprime(x):
+    result = True
+    for i in range(2, x):
+        if x % i == 0:
+            result = False
+            return result
+    return result
+
+def prime_nth(n):
+    counter, total = 1, 1
+    while counter <= n:
+        total += 1
+        if isprime(total):
+            counter += 1
+        # print(counter, total)
+    return total
+
+
+if __name__ == "__main__":
+    print(prime_nth(10001))
