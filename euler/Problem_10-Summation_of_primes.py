d8afd7cdae66f946ce8a2ce98ff97fd3f75ba2dd
Mingyyy <j.yanming@gmail.com>
2019-08-01 10:11:34 +0200

    Add initial Problem_11

--- euler/Problem_10-Summation_of_primes.py
+++ euler/Problem_10-Summation_of_primes.py
@@ -2,3 +2,55 @@
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 """
+import time
+
+
+def isprime(x):
+    result = True
+    for i in range(2, x):
+        if x % i == 0:
+            result = False
+            return result
+    return result
+
+
+def prime_sum(m, n):
+    s = 0
+    for i in range(m, n):
+        if isprime(i):
+            s += i
+    return s
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(prime_sum(1200001, 1500000))
+    print(f'--{time.time() - start_time} seconds--')
+
+# from 2, 50,000
+# 121013308
+# --11.624578714370728 seconds--
+
+# from 50,001 a 100, 000
+# 333383229
+# --31.92916774749756 seconds--
+
+# from 100,001 a 200, 000
+# 1255204276
+# --119.49474620819092 seconds--
+
+# from 200,001 a 500, 000
+# 8204635382
+# --773.7777619361877 seconds--
+
+# from 500,001 a 1,000, 000
+# 27636165828
+# --12838.361411094666 seconds--
+
+# from 1,000,001 a 1,100, 000
+# 7575351672
+# --2791.387584924698 seconds--
+
+# from 1,100,001 a 1,200, 000
+# 8307652436
+# --782.2548451423645 seconds--
