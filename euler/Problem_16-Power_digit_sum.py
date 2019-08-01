465de7588b42bca7d9516da9532f98df1db53444
Mingyyy <j.yanming@gmail.com>
2019-08-01 13:59:27 +0200

    Add Problem_16

--- euler/Problem_16-Power_digit_sum.py
+++ euler/Problem_16-Power_digit_sum.py
@@ -1,4 +1,13 @@
 """ 
-215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-What is the sum of the digits of the number 21000?
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+What is the sum of the digits of the number 2^1000?
 """
+
+
+i = 1000
+s = 0
+x = 2 ** i
+for j in str(x):
+    s += int(j)
+print(s)
+# 1366
