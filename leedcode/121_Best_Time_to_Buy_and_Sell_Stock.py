78c0c6a946b4be78fde5931edb990c821e5d33d1
Mingyyy <j.yanming@gmail.com>
2019-08-21 10:33:05 +0200

    update leedcode 121, need to improve performance

--- leedcode/121_Best_Time_to_Buy_and_Sell_Stock.py
+++ leedcode/121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -8,22 +8,25 @@ def maxProfit(prices):
         buy, sell = prices[0], prices[1]
         p = max(sell - buy, 0)
         for i in range(1, len(prices)-1):
-            if sell < prices[i+1]:
-                sell = prices[i+1]
-                if p < (prices[i+1]-prices[i]) and buy > prices[i]:
+            if max(prices[i+1:]) - min(prices[i], buy) > p:
+                if buy > prices[i]:
                     buy = prices[i]
+                sell = max(prices[i+1:])
             p = max(sell - buy, 0)
-            # print(i, p)
+            # print(i, p, sell, buy)
         return p
 
 
 if __name__ == "__main__":
-    print(maxProfit([2,1,2,0,1]))
+    print(maxProfit([3,3,5,0,0,3,1,4]))
+    print(maxProfit([2, 1, 2, 1, 0, 1, 2]))
     print(maxProfit([30,20,10,29,12,9,8,15,7]))
     print(maxProfit([7,1,5,3,6,4]))
     print(maxProfit([]))
     print(maxProfit([9]))
     print(maxProfit([8,2]))
     print(maxProfit([1,3]))
+    print(maxProfit([1,2,4]))
     print(maxProfit([3,1,2]))
     print(maxProfit([20,1,9,10,2,12]))
+    print(maxProfit([1,4,7,2]))
