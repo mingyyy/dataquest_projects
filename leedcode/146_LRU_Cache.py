716c653922dfbb236c2771b16414ad122eaf55dc
Mingyyy <j.yanming@gmail.com>
2019-08-28 12:33:22 +0100

    Update for 146, version 3  but still exceed the time limit

--- leedcode/146_LRU_Cache.py
+++ leedcode/146_LRU_Cache.py
@@ -5,37 +5,46 @@ class LRUCache:
         self.cache = {}
         self.pos = {}
 
-    def pos_key(self, last: bool) -> int:
-            # last = True -> get key with the highest score (last used)
-            # last = False -> get the least recent used key
-
-            if len(self.pos) > 0:
-                sorted_pos = sorted(self.pos.items(), key=lambda kv: kv[1])
-                if last is True:
-                    highest_score = sorted_pos[-1][1]
-                    return highest_score
-                else:
-                    key = sorted_pos[0][0]
-                    return key
-            else:
-                return 0
-
     def get(self, key: int) -> int:
         if key in self.cache:
-            k = self.pos_key(last=True)
-            self.pos[key] = k + 1
+            base_value = 0
+            for v in self.pos.values():
+                if v > base_value:
+                    base_value = v
+            self.pos[key] = base_value + 1
             return self.cache[key]
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
-        if len(self.cache) == self.capacity and key not in self.cache:
-            least_key = self.pos_key(last=False)
-            self.cache[key] = self.cache.pop(least_key)
-            self.pos[key] = self.pos.pop(least_key)
-        k = self.pos_key(last=True)
-        self.pos[key] = k + 1
-        self.cache[key] = value
+        if len(self.cache) == 0:
+            self.cache[key] = value
+            self.pos[key] = 0
+        else:
+            counter = 0
+            for k, v in self.pos.items():
+                if counter == 0:
+                    low, high, low_key = v, v, k
+                else:
+                    if v > high:
+                        high = v
+                    if v < low:
+                        low = v
+                        low_key = k
+                counter += 1
+
+            if key in self.cache:
+                self.cache[key] = value
+                self.pos[key] = high + 1
+            else:
+                if len(self.cache) < self.capacity:
+                    self.cache[key] = value
+                    self.pos[key] = high + 1
+                else:
+                    self.cache[key] = self.cache.pop(low_key)
+                    self.pos[key] = self.pos.pop(low_key)
+                    self.cache[key] = value
+                    self.pos[key] = high + 1
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
@@ -47,3 +56,6 @@ class LRUCache:
 
 # ["LRUCache","put","put","get","put","put","get"]
 # [[2],[2,1],[2,2],[2],[1,1],[4,1],[2]]
+
+# ["LRUCache","put","get"]
+# [[1],[2,1],[1]]
