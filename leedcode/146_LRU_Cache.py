6e91a71832910f6e33e5da33d2e14b942f653e47
Mingyyy <j.yanming@gmail.com>
2019-08-29 22:29:54 +0100

    Update for 146, version 3, accepted

--- leedcode/146_LRU_Cache.py
+++ leedcode/146_LRU_Cache.py
@@ -1,50 +1,27 @@
+from collections import OrderedDict as od
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
-        self.pos = {}
+        self.cache = od()
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            base_value = 0
-            for v in self.pos.values():
-                if v > base_value:
-                    base_value = v
-            self.pos[key] = base_value + 1
+            self.cache.move_to_end(key)
             return self.cache[key]
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
-        if len(self.cache) == 0:
+        if key in self.cache:
+            self.cache.move_to_end(key)
             self.cache[key] = value
-            self.pos[key] = 0
         else:
-            counter = 0
-            for k, v in self.pos.items():
-                if counter == 0:
-                    low, high, low_key = v, v, k
-                else:
-                    if v > high:
-                        high = v
-                    if v < low:
-                        low = v
-                        low_key = k
-                counter += 1
+            if len(self.cache) == self.capacity:
+                self.cache.popitem(False)
+            self.cache[key] = value
 
-            if key in self.cache:
-                self.cache[key] = value
-                self.pos[key] = high + 1
-            else:
-                if len(self.cache) < self.capacity:
-                    self.cache[key] = value
-                    self.pos[key] = high + 1
-                else:
-                    self.cache[key] = self.cache.pop(low_key)
-                    self.pos[key] = self.pos.pop(low_key)
-                    self.cache[key] = value
-                    self.pos[key] = high + 1
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
@@ -59,3 +36,13 @@ class LRUCache:
 
 # ["LRUCache","put","get"]
 # [[1],[2,1],[1]]
+
+# ["LRUCache","put","put","get","put","get","put","get","get","get", "put", "put", "put", "get"]
+# [[4],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4],[5,50], [3, 30], [3, 34], [5] ]
+
+
+# ["LRUCache","get","put","put","get","put","put","get","get"]
+# [[2],[2],[2,6],[2,5], [1],[1,5],[1,2],[1],[2]]
+#
+# ["LRUCache","put","put","put","put","get","get"]
+# [[2],[2,1],[1,1],[2,3],[4,1],[1],[2]]
