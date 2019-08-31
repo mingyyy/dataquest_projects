4fe3c5548f0bc0fda7cdf2bb9e1909f467c0d943
Mingyyy <j.yanming@gmail.com>
2019-08-31 18:59:48 +0100

    Leetcode serialize() attempt 1

--- leedcode/297_Serialize_and_Deserialize_Binary_Tree.py
+++ leedcode/297_Serialize_and_Deserialize_Binary_Tree.py
@@ -5,7 +5,24 @@
 #         self.left = None
 #         self.right = None
 
+
 class Codec:
+    def get_next_level(self, root):
+        # return the number of next level items, and a list [item1, item2]
+        n = 0
+        l = []
+        if root.left:
+            l.append(root.left)
+            n += 1
+        else:
+            l.append("null")
+
+        if root.right:
+            l.append(root.right)
+            n += 1
+        else:
+            l.append("null")
+        return n, l
 
     def serialize(self, root):
         """Encodes a tree to a single string.
@@ -13,6 +30,20 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
+        lc_tree, next = [root.val], [root, 0]
+        # root is at level 1, root has only one item at the same level
+        # when level=2, max_items = 2^(level-1)
+        level = 1
+
+        for i in range(0, 2**(level-1)):
+            root = next[i]
+            if self.get_next_level(root)[0] != 0:
+                for i in self.get_next_level(root)[1]:
+                    next.append(i)
+                    lc_tree.append(i)
+
+            level += 1
+
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
