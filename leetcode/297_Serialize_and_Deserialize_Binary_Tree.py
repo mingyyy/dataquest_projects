564f1b41baeb90613fba1ebe473159705167af5d
Mingyyy <j.yanming@gmail.com>
2019-09-03 10:00:04 -0400

    Modify serialize() function

--- leetcode/297_Serialize_and_Deserialize_Binary_Tree.py
+++ leetcode/297_Serialize_and_Deserialize_Binary_Tree.py
@@ -31,22 +31,48 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        lc_tree, next = [root.val], []
+        lc_tree = [root.val]
         # root is at level 1, root has only one item at the same level
         # when level=2, max_items = 2^(level-1)
         level = 1
+        next = []
+        fin = False
 
-        for i in range(0, 2**(level-1)):
-            if next:
-                root = next[i]
+        while fin is False:
+            current = next
+            next = []
+            has_next = False
+            i, node = 0, 0
+            while i < 2 ** (level - 1) or has_next is False:
+                if current:
+                    root = current[i]
+                if self.get_next_level(root)[0] != 0:
+                    has_next = True
+                i += 1
 
-            if self.get_next_level(root)[0] != 0:
-                for i in self.get_next_level(root)[1]:
-                    next.append(i)
-                    lc_tree.append(i)
+            if has_next is True:
+                for i in range(0, 2 ** (level - 1)):
 
-            if len(next) != 0:
-                level += 1
+                    if current:
+                        root = current[i]
+                    if self.get_next_level(root)[0] != 0:
+                        for item in self.get_next_level(root)[1]:
+                            next.append(item)
+                            lc_tree.append(item.val)
+                    else:
+                        lc_tree.append(None)
+                        lc_tree.append(None)
+            level += 1
+
+            print(level, lc_tree)
+
+            for x in next:
+                if x.left == None and x.right == None:
+                    node += 1
+            if node == len(next):
+                fin = True
+        return lc_tree
+
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
