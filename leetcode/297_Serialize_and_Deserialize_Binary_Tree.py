ec55c4493beb464787e16f5b5f979d55661fcc07
Mingyyy <j.yanming@gmail.com>
2019-09-03 15:21:35 -0400

    Update serialize() function

--- leetcode/297_Serialize_and_Deserialize_Binary_Tree.py
+++ leetcode/297_Serialize_and_Deserialize_Binary_Tree.py
@@ -5,25 +5,7 @@
 #         self.left = None
 #         self.right = None
 
-
 class Codec:
-
-    def get_next_level(self, root):
-        # return the number of next level items, and a list [item1, item2]
-        n = 0
-        l = []
-        if root.left:
-            l.append(root.left)
-            n += 1
-        else:
-            l.append("null")
-
-        if root.right:
-            l.append(root.right)
-            n += 1
-        else:
-            l.append("null")
-        return n, l
 
     def serialize(self, root):
         """Encodes a tree to a single string.
@@ -35,8 +17,8 @@ class Codec:
         # root is at level 1, root has only one item at the same level
         # when level=2, max_items = 2^(level-1)
         level = 1
-        next = []
         fin = False
+        next = []
 
         while fin is False:
             current = next
@@ -46,32 +28,64 @@ class Codec:
             while i < 2 ** (level - 1) or has_next is False:
                 if current:
                     root = current[i]
-                if self.get_next_level(root)[0] != 0:
-                    has_next = True
+
+                if root != None:
+                    if self.get_next_level(root)[0] != 0:
+                        has_next = True
                 i += 1
 
             if has_next is True:
                 for i in range(0, 2 ** (level - 1)):
-
                     if current:
                         root = current[i]
                     if self.get_next_level(root)[0] != 0:
                         for item in self.get_next_level(root)[1]:
                             next.append(item)
-                            lc_tree.append(item.val)
+                            if item == None:
+                                lc_tree.append(None)
+                            else:
+                                lc_tree.append(item.val)
                     else:
-                        lc_tree.append(None)
-                        lc_tree.append(None)
+                        pass
+
             level += 1
-
-            print(level, lc_tree)
-
             for x in next:
-                if x.left == None and x.right == None:
+                if x == None:
+                    node += 1
+                elif x.left == None and x.right == None:
                     node += 1
             if node == len(next):
                 fin = True
+
+            # print(fin, level, lc_tree, next)
+        n = len(lc_tree)
+        while lc_tree[n - 1] == None:
+            lc_tree.pop()
+            n = len(lc_tree)
+
+        print(lc_tree)
         return lc_tree
+
+    def get_next_level(self, root):
+        # return the number of next level items, and a list [item1, item2]
+        n = 0
+        l = []
+        if root == None:
+            n = 0
+        else:
+            if root.left:
+                l.append(root.left)
+                n = 1
+            else:
+                l.append(None)
+
+            if root.right != None:
+                l.append(root.right)
+                n = 1
+            else:
+                l.append(None)
+
+        return n, l
 
 
     def deserialize(self, data):
